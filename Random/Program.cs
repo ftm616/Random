2835b729fdbc9f7834ff15057af0240d99073224
ftm616 <ftm.mhmdi.006@gmail.com>
2025-12-06 09:09:19 +0330

    Add project files.

---
+++ Random/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace appRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Random rnd = new Random();
+
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.Next(1, 101); 
+        }
+
+        Console.WriteLine("مقادیر تصادفی آرایه:");
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.ReadKey();
+    
+        }
+    }
+}
